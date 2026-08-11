44feeee2f8d3377715302805150b3d3913e12f4b
DHthe1ST <s.husyndms@gmail.com>
2026-08-11 08:29:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="errandly-v0.6.1";
+const CACHE_NAME="errandly-v0.6.2";
 const ASSETS=["./","./index.html","./account.html","./admin.html","./style.css","./app.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
